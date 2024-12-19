a2cab5684af21f9a8cbce411ac507d317c0b6130
Shruthi V <shruthivshruthiv8@gmail.com>
2024-12-19 20:24:05 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+$(document).ready(function () {
+    $('#registrationForm').on('submit', function (event) {
+        event.preventDefault(); // Prevent default form submission
+
+        const formData = $(this).serialize(); // Serialize the form data
+        window.location.href = 'complete.html?' + formData; // Redirect to the Complete page with query parameters
+    });
+});
